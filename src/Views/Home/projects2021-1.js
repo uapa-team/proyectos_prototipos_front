11597368c548e1319191db916f0795d52b62c97d
Jhoan Restrepo <jsr.etc@hotmail.com>
2021-09-20 10:47:40 -0500

    2021-2

---
+++ src/Views/Home/projects2021-1.js
@@ -0,0 +1,132 @@
+export default [
+  {
+    Name: "Mermaids Dance team",
+    Description: "tgutierrezr@unal.edu.co 312 536 25 74",
+    Video_Key: "utv0QBPFH5M",
+  },
+  {
+    Name: "MIND Rover",
+    Description: "dfzunigah@unal.edu.co instagram: @mind_unal",
+    Video_Key: "RjweFXz_zzQ",
+  },
+  {
+    Name: "Ingenia tu Camino - RAMA IEEE-",
+    Description:
+      "estieee_fibog@unal.edu.co www.ingenieria.bogota.unal.edu.co/rama-ieee/",
+    Video_Key: "0t5lqMtMPhE",
+  },
+  {
+    Name: "Grupo patinaje APUN",
+    Description: "diacastroco@unal.edu.co",
+    Video_Key: "Qohf6-TRVeU",
+  },
+  {
+    Name: "OUT - XV",
+    Description: "eforeroc@unal.edu.co; out_bog@unal.edu.co",
+    Video_Key: "gGq374bonh0",
+  },
+  {
+    Name: "EduHerramientasING _proyecto eléctrica-",
+    Description:
+      "Instagram @proyectoelectrica En facebook @proyectoelectrica y @CorrienteAlternaUN peun_fibog@unal.edu.co",
+    Video_Key: "07W8QJ9fF0I",
+  },
+  {
+    Name: "Corriente Alterna",
+    Description:
+      "Instagram @proyectoelectrica En facebook @proyectoelectrica y @CorrienteAlternaUN peun_fibog@unal.edu.co",
+    Video_Key: "cbBXHmSQRfA",
+  },
+  {
+    Name: "Sharky - STEOS-",
+    Description: "steos@unal.edu.co",
+    Video_Key: "iX7hspwCGaI",
+  },
+  {
+    Name: "II CNVE 2020 - STEOS-",
+    Description: "iicnvte@unal.edu.co",
+    Video_Key: "SJX1Lq9uZ6U",
+  },
+  {
+    Name: "CHEM-E-CAR UNAL BOG",
+    Description: "aiche_fibog@unal.edu.co",
+    Video_Key: "i7Sjg4j8ShU",
+  },
+  {
+    Name: "Softroboticsun",
+    Description: "Facebook es Softroboticsun softrobot_fibog@unal.edu.co",
+    Video_Key: "rh7fC_xB5bA",
+  },
+  {
+    Name: "UNRobot",
+    Description: "ceimtun@unal.edu.co",
+    Video_Key: "8tJ0dYauiKk",
+  },
+  {
+    Name: "MMA-UN 2020",
+    Description: "msalgadoh@unal.edu.co",
+    Video_Key: "YiOZUG4fmic",
+  },
+  {
+    Name: "Producción de Rúgula - AMARANTO-",
+    Description:
+      "amaranto_fibog@unal.edu.co jsagudeloac@unal.edu.co instagram @amarantoun",
+    Video_Key: "P2WL0tPO_v0",
+  },
+  {
+    Name: "Radio Nachonal",
+    Description: "mabastidasg@unal.edu.co",
+    Video_Key: "OwWSpWbpv4o",
+  },
+  {
+    Name: "BioMed School UN",
+    Description: "joahernandezmo@unal.edu.co",
+    Video_Key: "QMf5RasOCCg",
+  },
+  {
+    Name: "Cohete Eos- GIDA UN-",
+    Description:
+      "gida_fibog@unal.edu.co Facebook: https://www.facebook.com/GIDAUN Twitter: https://twitter.com/gidaun?lang=es",
+    Video_Key: "Us9mihFhkSg",
+  },
+  {
+    Name: "ElectricoYTelemetria- Grupo KartUN-",
+    Description: "kartun_fibog@unal.edu.co",
+    Video_Key: "_UQh8lWe_Oo",
+  },
+  {
+    Name: "Phemi UNAL-Fase III",
+    Description: "caphemi_fdbog@unal.edu.co",
+    Video_Key: "dGUCZNLDSmU",
+  },
+  {
+    Name: "Aerospace Race- AESS UN-",
+    Description: "aess_fibog@unal.edu.co",
+    Video_Key: "R94uNUJLW0I",
+  },
+  {
+    Name: "Owl´s Dance Crew2020",
+    Description: "mcarmonac@unal.edu.co",
+    Video_Key: "y9fVJUR3drg",
+  },
+  {
+    Name: "Prototipo estación- GRISEC-",
+    Description: "sgrisec_bog@unal.edu.co",
+    Video_Key: "4Te44apCEM0",
+  },
+  {
+    Name: "Un Akibakei 20",
+    Description: "unakiba_fchbog@unal.edu.co",
+    Video_Key: "f5UDa2uCeUQ",
+  },
+  {
+    Name: "Sociedad de Debate",
+    Description: "sdebate_bog@unal.edu.co",
+    Video_Key: "d9s7jnA5gdI",
+  },
+  {
+    Name: "Cuenteria en la UN",
+    Description: "bardos_fcbog@unal.edu.co",
+    Video_Key: "Q2FRQ4CGrZk",
+  },
+];
