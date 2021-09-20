6ae3c24214a20e7d080a184aaf1f911433b5d6b4
Daniel Escobar <daescobarp@unal.edu.co>
2021-09-20 15:01:49 +0000

    message

--- src/Views/Home/projects.js
+++ src/Views/Home/projects.js
@@ -1,302 +1,132 @@
 export default [
   {
-    Name: "PROYECTO ELÉCTRICA",
+    Name: "Mermaids Dance team",
+    Description: "tgutierrezr@unal.edu.co 312 536 25 74",
+    Video_Key: "utv0QBPFH5M",
+  },
+  {
+    Name: "MIND Rover",
+    Description: "dfzunigah@unal.edu.co instagram: @mind_unal",
+    Video_Key: "RjweFXz_zzQ",
+  },
+  {
+    Name: "Ingenia tu Camino - RAMA IEEE-",
     Description:
-      "Proyecto que ofrecer talleres de proyectos interdisciplinares",
-    Poster_Key: "",
-    Video_Key: "1w8c4Mpl2x0",
+      "estieee_fibog@unal.edu.co www.ingenieria.bogota.unal.edu.co/rama-ieee/",
+    Video_Key: "0t5lqMtMPhE",
   },
   {
-    Name: "CORRIENTE ALTERNA:",
+    Name: "Grupo patinaje APUN",
+    Description: "diacastroco@unal.edu.co",
+    Video_Key: "Qohf6-TRVeU",
+  },
+  {
+    Name: "OUT - XV",
+    Description: "eforeroc@unal.edu.co; out_bog@unal.edu.co",
+    Video_Key: "gGq374bonh0",
+  },
+  {
+    Name: "EduHerramientasING _proyecto eléctrica-",
     Description:
-      "Periodico estudiantil, hecho para los estudiantes por los estudiantes",
-    Poster_Key: "",
-    Video_Key: "yzA8OdJz4R0",
+      "Instagram @proyectoelectrica En facebook @proyectoelectrica y @CorrienteAlternaUN peun_fibog@unal.edu.co",
+    Video_Key: "07W8QJ9fF0I",
   },
   {
-    Name: "OUT",
+    Name: "Corriente Alterna",
     Description:
-      "Proyecto que organiza las Olimpiadas Universitarias de Termodinámica",
-    Poster_Key: "",
-    Video_Key: "89q7rHyXAf4",
+      "Instagram @proyectoelectrica En facebook @proyectoelectrica y @CorrienteAlternaUN peun_fibog@unal.edu.co",
+    Video_Key: "cbBXHmSQRfA",
   },
   {
-    Name: "Dron y satélite Agro",
+    Name: "Sharky - STEOS-",
+    Description: "steos@unal.edu.co",
+    Video_Key: "iX7hspwCGaI",
+  },
+  {
+    Name: "II CNVE 2020 - STEOS-",
+    Description: "iicnvte@unal.edu.co",
+    Video_Key: "SJX1Lq9uZ6U",
+  },
+  {
+    Name: "CHEM-E-CAR UNAL BOG",
+    Description: "aiche_fibog@unal.edu.co",
+    Video_Key: "i7Sjg4j8ShU",
+  },
+  {
+    Name: "Softroboticsun",
+    Description: "Facebook es Softroboticsun softrobot_fibog@unal.edu.co",
+    Video_Key: "rh7fC_xB5bA",
+  },
+  {
+    Name: "UNRobot",
+    Description: "ceimtun@unal.edu.co",
+    Video_Key: "8tJ0dYauiKk",
+  },
+  {
+    Name: "MMA-UN 2020",
+    Description: "msalgadoh@unal.edu.co",
+    Video_Key: "YiOZUG4fmic",
+  },
+  {
+    Name: "Producción de Rúgula - AMARANTO-",
     Description:
-      "Proyecto que busca la capacitación en teledetección aplicada a la agricultura de precisión con datos de satélite y drones.",
-    Poster_Key: "",
-    Video_Key: "hyh9T11XgtA",
+      "amaranto_fibog@unal.edu.co jsagudeloac@unal.edu.co instagram @amarantoun",
+    Video_Key: "P2WL0tPO_v0",
   },
   {
-    Name: "MMA-UN",
-    Description: "proyecto de Artes Marciales Mixtas",
-    Poster_Key: "",
-    Video_Key: "wrRlG-y_TG4",
+    Name: "Radio Nachonal",
+    Description: "mabastidasg@unal.edu.co",
+    Video_Key: "OwWSpWbpv4o",
   },
   {
-    Name: "ONUNAL",
+    Name: "BioMed School UN",
+    Description: "joahernandezmo@unal.edu.co",
+    Video_Key: "QMf5RasOCCg",
+  },
+  {
+    Name: "Cohete Eos- GIDA UN-",
     Description:
-      "Proyecto que busca  Asistir al modelo de Naciones Unidas de la Universidad Milita",
-    Poster_Key: "",
-    Video_Key: "gju2gWHy7aw",
+      "gida_fibog@unal.edu.co Facebook: https://www.facebook.com/GIDAUN Twitter: https://twitter.com/gidaun?lang=es",
+    Video_Key: "Us9mihFhkSg",
   },
   {
-    Name: "BIOMED UN",
-    Description:
-      "Proyecto sobre Sesión de técnicas en biología celular,  EEG, Emotiv Insight y BCI",
-    Poster_Key: "",
-    Video_Key: "C50M0GF0lxA",
+    Name: "ElectricoYTelemetria- Grupo KartUN-",
+    Description: "kartun_fibog@unal.edu.co",
+    Video_Key: "_UQh8lWe_Oo",
   },
   {
-    Name: "OWLS CREW",
-    Description: "Proyecto de danza urbana",
-    Poster_Key: "",
-    Video_Key: "oqlkVQ_3Zj0",
+    Name: "Phemi UNAL-Fase III",
+    Description: "caphemi_fdbog@unal.edu.co",
+    Video_Key: "dGUCZNLDSmU",
   },
   {
-    Name: "APUN",
-    Description: "Proyecto de patinaje",
-    Poster_Key: "",
-    Video_Key: "ft5A8h50HAU",
+    Name: "Aerospace Race- AESS UN-",
+    Description: "aess_fibog@unal.edu.co",
+    Video_Key: "R94uNUJLW0I",
   },
   {
-    Name: "capitulo AiCHE - CHEM-E-CAR UN",
-    Description:
-      "proyecto de CHEM-E -CAR  (este proyecto ensambla un carro que se conduce a partir de reacciones quimicas),  tecnologías de rehabilitación.",
-    Poster_Key: "",
-    Video_Key: "aJh8jAqVrfE",
+    Name: "Owl´s Dance Crew2020",
+    Description: "mcarmonac@unal.edu.co",
+    Video_Key: "y9fVJUR3drg",
   },
   {
-    Name: "RAMA IEEE",
-    Description:
-      "Proyecto de Ingenerios eléctricos, eléctronicos que busca realizar  construcción de un robot explorador con transmisión de Poster_Key, con el objetivo de aplicar los conocimientos de la ingeniería.",
-    Poster_Key: "bBD9VLx",
-    Video_Key: "Xka8hbBhWzM",
+    Name: "Prototipo estación- GRISEC-",
+    Description: "sgrisec_bog@unal.edu.co",
+    Video_Key: "4Te44apCEM0",
   },
   {
-    Name: "CEIMTUN",
-    Description: "Proyecto de desarrollo y difusión en robótica",
-    Poster_Key: "",
-    Video_Key: "SCmfdipKk3U",
+    Name: "Un Akibakei 20",
+    Description: "unakiba_fchbog@unal.edu.co",
+    Video_Key: "f5UDa2uCeUQ",
   },
   {
-    Name: "STEOS",
-    Description:
-      "Proyecto que busca el ensamble y puesta en marcha del prototipo de vehículo eléctrico",
-    Poster_Key: "",
-    Video_Key: "OeTq0yIz5y4",
+    Name: "Sociedad de Debate",
+    Description: "sdebate_bog@unal.edu.co",
+    Video_Key: "d9s7jnA5gdI",
   },
   {
-    Name: "AKIBAKEI",
-    Description:
-      "Proyecto sobre el anime, el manga y juegos de Roll entre otras actividades",
-    Poster_Key: "",
-    Video_Key: "ueV8GCUAm8M",
-  },
-  {
-    Name: "COMUNIDAD ACADÉMICA PHEMI",
-    Description: "Talleres de creatividad, expresión oral, expresión corporal",
-    Poster_Key: "",
-    Video_Key: "__geHI_zySg",
-  },
-  {
-    Name: "RADIO NACHONAL",
-    Description: "Proyecto de emisora universitaria",
-    Poster_Key: "",
-    Video_Key: "DzFGdZrgz8c",
-  },
-  {
-    Name: "AAFIN",
-    Description:
-      "Proyecto de academia de árbitros Facultad de Ingeniería, Dentro del desarrollo de las  actividades que  encierran a este proyecto se generan  proceso de formación teórica que se llevará a cabo  de manera paralela a la formación práctica en campo durante el desarrollo del Torneo de Integración Facultad Ingeniería,",
-    Poster_Key: "",
-    Video_Key: "F51zKfDFaZ0",
-  },
-  {
-    Name: "MIND ROVER",
-    Description:
-      "Proyecto que busca desarrollar un vehículo que cumpla con los parámetros exigidos para participar en la convocatoria de la NASSA 2021",
-    Poster_Key: "",
-    Video_Key: "p-MNaX2Cty0",
-  },
-  {
-    Name: "AMARANTO",
-    Description: "Proyecto de siembra de Rúgula",
-    Poster_Key: "",
-    Video_Key: "Vd74OmhACKE",
-  },
-  {
-    Name: "GIDA",
-    Description: "Grupo de Investigación y Desarrollo Aeroespacial",
-    Poster_Key: "",
-    Video_Key: "tnL21aDCMrQ",
-  },
-  {
-    Name: "SoftroboticsUN",
-    Description: "Proyecto de robótica flexible",
-    Poster_Key: "",
-    Video_Key: "u2qGYvt9KEM",
-  },
-  {
-    Name: "KartUN",
-    Description: "Proyecto de ensamble y puesta en marcha de carros tipo Kart",
-    Poster_Key: "",
-    Video_Key: "ngBQR-EMRAc",
-  },
-  {
-    Name: "Team-Mermaids",
-    Description: "Proyecto de porras modalidad de cheer.",
-    Poster_Key: "",
-    Video_Key: "dtUEi86a0rg",
-  },
-  {
-    Name: "TALLER DE GUITARRA (Nivel básico e intermedio)",
-    Description:
-      "El taller de guitarra tiene como objetivo general desarrollar en los participantes un lenguaje musical instrumental a partir de fundamentos teóricos principales, técnica básica y repertorios de canciones con pedagogía teórico-práctica, contribuyendo a la integración del personal y la utilización del tiempo libre en actividad. Horarios para el periodo 2020-03. Nivel básico: jueves 4 - 6 p.m. Nivel intermedio: viernes 4 - 6 p.m.",
-    Poster_Key: "",
-    Video_Key: "wrWK3DxkTOU",
-  },
-  {
-    Name: "TALLER DE EXPRESIÓN CORPORAL Y TEATRO",
-    Description:
-      "El taller de expresión corporal y teatro tiene como propósito brindar herramientas y desarrollar en los participantes un lenguaje escénico a través de fundamentos teórico-prácticos y procesos creativos que permitan potenciar las habilidades expresivas y relacionales, promoviendo un ambiente lúdico, de autoconfianza, tranquilidad, colaboración y respeto.",
-    Poster_Key: "",
-    Video_Key: "9qFlo4v1cm8",
-  },
-  {
-    Name:
-      "Área de Cultura Programa Actividad Lúdico - Cultural Café de Lenguas",
-    Description:
-      "Café de Lenguas virtual en GoogleMeet: Francés, Inglés, Alemán, Portugués, Italiano. Actividad Lúdico-Cultural. Viernes Culturales en Ingeniería. Participan invitados extranjeros y estudiantes de intercambio internacional. Para mayor información, consultar los boletínes virtuales y la página del área de cultura: https://ingenieria.bogota.unal.edu.co/es/dependencias/direccion-de-bienestar/area-de-cultura.html",
-    Poster_Key: "dZKBzKE",
-    Video_Key: "nvk8781",
-  },
-  {
-    Name:
-      "Área de Cultura Programa de Expresión de Talentos Ingeniería UN Concursos, Festivales, Retos, Muestras Artísticas",
-    Description:
-      "Programa de Expresión de Talentos Para mayor información, consultar la página del área de cultura y el módulo de Galería: https://ingenieria.bogota.unal.edu.co/es/dependencias/direccion-de-bienestar/area-de-cultura.html https://ingenieria.bogota.unal.edu.co/es/dependencias/direccion-de-bienestar/galeria.html",
-    Poster_Key: "WGmVKqB",
-    Video_Key: "nvk8782",
-  },
-  {
-    Name:
-      "Área de Cultura Programa Actividad Lúdico - Cultural Talleres de Formación Artística",
-    Description:
-      "Talleres cortos de formación artística y estética: Guitarra, Expresión Corporal y Teatro, Producción Audiovisual, Fotografía, entre otros. Para mayor información, consultar los boletínes virtuales y la página del área de cultura: https://ingenieria.bogota.unal.edu.co/es/dependencias/direccion-de-bienestar/area-de-cultura.html",
-    Poster_Key: "OoQfsaq",
-    Video_Key: "nvk8783",
-  },
-  {
-    Name: "Area de Acompañamiento Integral",
-    Description:
-      "Acompañamiento a estudiantes PEAMA - Programa especial de admisión para la movilidad académica-Ruta PEAMA",
-    Poster_Key: "",
-    Video_Key: "MgtiqwYcTVA",
-  },
-  {
-    Name: "Area de Acompañamiento Integral",
-    Description:
-      "Acompañamiento a estudiantes PEAMA - Programa especial de admisión para la movilidad académica-Portabilidad PEAMA",
-    Poster_Key: "",
-    Video_Key: "QdcLE75P9v4",
-  },
-  {
-    Name: "Area de Acompañamiento Integral",
-    Description:
-      "Acompañamiento a estudiantes PEAMA - Programa especial de admisión para la movilidad académica-Bienvenida PEAMA Orinoquia",
-    Poster_Key: "",
-    Video_Key: "e8KnSceBbqU",
-  },
-  {
-    Name: "Area de Acompañamiento Integral",
-    Description:
-      "Acompañamiento a estudiantes PEAMA - Programa especial de admisión para la movilidad académica-Bienvenida PEAMA Tumaco",
-    Poster_Key: "",
-    Video_Key: "12CDe_l-X9g",
-  },
-  {
-    Name: "Area de Acompañamiento Integral",
-    Description:
-      "Acompañamiento a estudiantes PEAMA - Programa especial de admisión para la movilidad académica-Bienvenida PEAMA Amazonas",
-    Poster_Key: "",
-    Video_Key: "yPushpfkbMw",
-  },
-  {
-    Name: "Area de Acompañamiento Integral",
-    Description:
-      "Acompañamiento a estudiantes PEAMA - Programa especial de admisión para la movilidad académica-Bienvenida programas especiales",
-    Poster_Key: "",
-    Video_Key: "B-V4zBpJsB4",
-  },
-  {
-    Name: "Area de Acompañamiento Integral",
-    Description:
-      "Acompañamiento a estudiantes PEAMA - Programa especial de admisión para la movilidad académica-Práctica Colombia",
-    Poster_Key: "",
-    Video_Key: "wgm0q-icXSU",
-  },
-  {
-    Name: "Bienvenida Bienestar",
-    Description:
-      "Dar la bienvenida a los estudiantes de nuevo ingreso y brindar la información sobre las actividades de bienestar universitario",
-    Poster_Key: "",
-    Video_Key: "WIVrYTS8Q04",
-  },
-  {
-    Name: "Bienvenidos a la Nacho",
-    Description:
-      "Reflexión sobre el cambio de perspectiva del colegio a la universidad y de la presencialidad a la virtualidad, generando el sentido de pertenencia que nos da la sensación de estar en un entorno o grupo de personas con las que podamos compartir un sinfín de sentimientos.",
-    Poster_Key: "",
-    Video_Key: "HowzDmhFGvc",
-  },
-  {
-    Name: "Tejido social virtual",
-    Description:
-      "Fomento de lazos de amistad entre los estudiantes admitidos en un entorno virtual mediante actividades dinámicas virtuales y la promoción del sticker UN abrazo virtual.",
-    Poster_Key: "YB2vifT",
-    Video_Key: "nvk8784",
-  },
-  {
-    Name: "Buenas prácticas en salud",
-    Description:
-      "Promoción de estrategias, actividades y tips que permitirán sobrellevar situaciones tensionantes causadas por diversos factores, tales como el estrés académico durante el semestre o el encierro durante la cuarentena, con el fin de llevar un estilo de vida saludable en medio de la quietud en casa.",
-    Poster_Key: "i5yDd0M",
-    Video_Key: "HXC8geLjoDI",
-  },
-  {
-    Name: "Cinco minutos de ética",
-    Description:
-      "Difusión de la ética en la cotidianidad en redes sociales. La Dirección de Bienestar propone un espacio de reflexión en aras de incentivar la formación integral de los estudiantes de ingeniería a través de 6 cápsulas de ética.",
-    Poster_Key: "dKOTOGo",
-    flag: true,
-    Video_Key: "I2GMCbWzIHQ",
-  },
-  {
-    Name: "Las mujeres y la ingeniería",
-    Description:
-      "Proyecto que tiene como finalidad motivar el ingreso de niñas y jóvenes a los estudios profesionales en ingeniería en la Facultad de Ingeniería de la Universidad Nacional de Colombia.",
-    Poster_Key: "f7A6SvE",
-    Video_Key: "nAAcSVuItpA",
-  },
-  {
-    Name: "Salud mental universitaria",
-    Description:
-      "Proyecto que promueve la salud mental positiva y el bienestar de los estudiantes en la Facultad de Ingeniería, adicionalmente se tienen sistemas de apoyo para estudiantes que requieren ayuda.",
-    Poster_Key: "2c5pnUe",
-    Video_Key: "nvk8785",
-  },
-  {
-    Name: "Agricultura urbana",
-    Description:
-      "Proyecto que brinda un espacio de sano compartir y aprendizaje donde se promueve la participación e interacción de diferentes personas en las prácticas y conocimientos relacionados con técnicas, formas de hacer y de aprovechar la agricultura urbana.",
-    Poster_Key: "t0E7xXL",
-    Video_Key: "nvk8786",
-  },
-  {
-    Name: "Aerospace Race",
-    Description:
-      "Proyecto estudiantil que abordar temas aerospaciales y trabaja con proyectos para aplicar el conocimiento teórico y hacerlo práctico, como el primer equipo universitario de drones de carrera Rover agrónomo y satélite en lata. Contacto: aess_fibog@unal.edu.co",
-    Video_Key: "lxXZFqErtBY",
+    Name: "Cuenteria en la UN",
+    Description: "bardos_fcbog@unal.edu.co",
+    Video_Key: "Q2FRQ4CGrZk",
   },
 ];
